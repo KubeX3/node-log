b2186c83e7d93224ebf4ede752751ce1b90ec239
Lahiru De Silva <lahirurashmitha00@gmail.com>
2026-02-04 14:30:00 +0530

    Update

--- bundle.js
+++ bundle.js
@@ -1,18 +1,19 @@
-const esbuild = require('esbuild');
+import esbuild from 'esbuild';
 
 esbuild.build({
-  entryPoints: ['src/index.ts'], // Express main process entry
+  entryPoints: ['src/index.ts'],
   outfile: 'dist/index.js',
   bundle: true,
   platform: 'node',
-  external: [], // keep native deps external
-  target: ['ES2023'],
+  format: 'esm',
+  packages: 'external',
+  target: ['ES2022'],
   sourcemap: false,
   minify: true,
 
 }).then(() => {
   console.log('Bundled successfully');
-
+  
 }).catch((err) => {
   console.error(err);
   process.exit(1);
