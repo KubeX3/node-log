8ee4c322356c7b322968f914e71ae8ad3af996ba
Lahiru De Silva <lahirurashmitha00@gmail.com>
2026-02-05 14:30:00 +0530

    Update logs with write logs on .log file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import path from "path";
+import { promises as fs } from "fs";
+import { DOTENV } from "./utils/dotenv";
 import { LogTypesEnum } from "./utils/enums/log-types.enum";
 
 // ANSI color codes
@@ -10,9 +13,30 @@ const colors = {
   RESET: "\x1b[0m",
 };
 
+/**
+ * Appends the log message to <log file>.log without ANSI colors
+ */
+async function logFile(
+  logWithColor: string,
+  logWithoutColor: string,
+): Promise<void> {
+  const filePath: string = DOTENV.LOG_FILE_PATH;
+
+  try {
+    // Ensure the directory exists if the path is nested (e.g., ./logs/system.log)
+    const dir = path.dirname(filePath);
+    await fs.mkdir(dir, { recursive: true });
+
+    await fs.appendFile(filePath, logWithoutColor + "\n");
+  } catch (error) {
+    console.error("Error handling file operation:", error);
+  }
+  console.log(logWithColor);
+}
+
+// Format date & time
 function getFormattedDateTime(): string {
   const now = new Date();
-
   const year = now.getFullYear();
   const month = String(now.getMonth() + 1).padStart(2, "0");
   const day = String(now.getDate()).padStart(2, "0");
@@ -20,67 +44,37 @@ function getFormattedDateTime(): string {
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
 
-  return `${colors.GREEN}[${year}/${month}/${day} - ${hours}:${minutes}:${seconds}]${colors.RESET}`;
+  return `[${year}-${month}-${day} - ${hours}:${minutes}:${seconds}]`;
 }
 
 function logError(message: string, location?: string): void {
-  console.log(
-    "%s %s[%s]%s%s - %s",
-    getFormattedDateTime(), // green timestamp
-    colors.RED, // red color start
-    LogTypesEnum.ERROR, // log level
-    location ? ` - [${location}]` : "", // optional location
-    colors.RESET, // reset color
-    message, // error message
-  );
+  const logWithColor: string = `${colors.GREEN}${getFormattedDateTime()} ${colors.RED}[${LogTypesEnum.ERROR}]${location ? `\t - [${location}]` : ""}${colors.RESET} - ${message}`;
+  const logWithoutColor: string = `${getFormattedDateTime()} [${LogTypesEnum.ERROR}]${location ? `\t - [${location}]` : ""} - ${message}`;
+  logFile(logWithColor, logWithoutColor);
 }
 
 function logWarning(message: string, location?: string): void {
-  console.log(
-    "%s %s[%s]%s%s - %s",
-    getFormattedDateTime(), // green timestamp
-    colors.YELLOW, // yellow color start
-    LogTypesEnum.WARNING, // log level
-    location ? ` - [${location}]` : "", // optional location
-    colors.RESET, // reset color
-    message, // info message
-  );
+  const logWithColor: string = `${colors.GREEN}${getFormattedDateTime()} ${colors.YELLOW}[${LogTypesEnum.WARNING}]${location ? `\t - [${location}]` : ""}${colors.RESET} - ${message}`;
+  const logWithoutColor: string = `${getFormattedDateTime()} [${LogTypesEnum.WARNING}]${location ? `\t - [${location}]` : ""} - ${message}`;
+  logFile(logWithColor, logWithoutColor);
 }
 
 function logInfo(message: string, location?: string): void {
-  console.log(
-    "%s %s[%s]%s%s - %s",
-    getFormattedDateTime(), // green timestamp
-    colors.GREEN, // green color start
-    LogTypesEnum.INFO, // log level
-    location ? ` - [${location}]` : "", // optional location
-    colors.RESET, // reset color
-    message, // info message
-  );
+  const logWithColor: string = `${colors.GREEN}${getFormattedDateTime()} ${colors.GREEN}[${LogTypesEnum.INFO}]${location ? `\t - [${location}]` : ""}${colors.RESET} - ${message}`;
+  const logWithoutColor: string = `${getFormattedDateTime()} [${LogTypesEnum.INFO}]${location ? `\t - [${location}]` : ""} - ${message}`;
+  logFile(logWithColor, logWithoutColor);
 }
 
 function logAudit(message: string, location?: string): void {
-  console.log(
-    "%s %s[%s]%s%s - %s",
-    getFormattedDateTime(), // green timestamp
-    colors.CYAN, // cyan color start
-    LogTypesEnum.AUDIT, // log level
-    location ? ` - [${location}]` : "", // optional location
-    colors.RESET, // reset color
-    message, // info message
-  );
+  const logWithColor: string = `${colors.GREEN}${getFormattedDateTime()} ${colors.CYAN}[${LogTypesEnum.AUDIT}]${location ? `\t - [${location}]` : ""}${colors.RESET} - ${message}`;
+  const logWithoutColor: string = `${getFormattedDateTime()} [${LogTypesEnum.AUDIT}]${location ? `\t - [${location}]` : ""} - ${message}`;
+  logFile(logWithColor, logWithoutColor);
 }
 
 function logEvent(message: string, location?: string): void {
-  console.log(
-    "%s %s[%s]%s%s - %s",
-    getFormattedDateTime(), // green timestamp
-    colors.MAGENTA, // magenta color start
-    LogTypesEnum.EVENT, // log level
-    location ? ` - [${location}]` : "", // optional location
-    colors.RESET, // reset color
-    message, // info message
-  );
+  const logWithColor: string = `${colors.GREEN}${getFormattedDateTime()} ${colors.MAGENTA}[${LogTypesEnum.EVENT}]${location ? `\t - [${location}]` : ""}${colors.RESET} - ${message}`;
+  const logWithoutColor: string = `${getFormattedDateTime()} [${LogTypesEnum.EVENT}]${location ? `\t - [${location}]` : ""} - ${message}`;
+  logFile(logWithColor, logWithoutColor);
 }
 
 export { logError, logWarning, logInfo, logAudit, logEvent };
---
+++ src/utils/dotenv.ts
@@ -0,0 +1,24 @@
+// dotenv-local.ts
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (process.env.NODE_ENV === "development") {
+  dotenv.config({ debug: true });
+}
+
+const now = new Date();
+console.log(
+  "\x1b[32m[%s/%s/%s - %s:%s:%s] \x1b[33m[INFO] - [dotenv-local.ts] - \x1b[37m%s",
+  now.getFullYear(),
+  String(now.getMonth() + 1).padStart(2, "0"),
+  String(now.getDate()).padStart(2, "0"),
+  String(now.getHours()).padStart(2, "0"),
+  String(now.getMinutes()).padStart(2, "0"),
+  String(now.getSeconds()).padStart(2, "0"),
+  "Dotenv inject",
+);
+
+export const DOTENV = {
+  LOG_FILE_PATH: process.env.LOG_FILE_PATH || "./logs/system.log",
+};
